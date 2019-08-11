4f8a0e5b828c73feee40529700a289d5576f4fda
liyuan <1250953354@qq.com>
2019-08-11 17:38:07 +0800

    头部工作已完成

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,8 +10,40 @@ class header extends Component {
                 {title:"悦单"},
                 {title:"娱乐"},
                 {title:"商城"},
-            ]
+            ],
+            top:[
+                {title:"最爱表达爱意的男团成员是"},
+                {title:'男爱豆"中毒身亡"现场'},
+                {title:"零零后男孩故事在线翻天"},
+                {title:"原来千禧年的爱豆是这样拍照的"},
+                {title:'可爱强东玥 在线"借钱"'},
+                {title:"豹笑新人女团来面试"},
+                {title:'新人"相声组"李聪&Veengee专访'},
+                {title:"男团面试当场刮彩票"},
+                {title:"李希侃晋升面试表情大"},
+                {title:"BOY STORY社内突袭访问"},
+            ],
+            inputValue:""
          };
+    }
+    inputChange=(e)=>{
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+    inputFous=(e)=>{
+        e.target.value=""
+        var centerinput= document.querySelector('.centerinput')
+        centerinput.style.backgroundColor="#fff"
+        var topelse=document.querySelector('.topelse')
+        topelse.style.display="block"
+        
+    }
+    inputBlur=(e)=>{
+        var centerinput= document.querySelector('.centerinput')
+        centerinput.style.backgroundColor="#dcdbdb"
+        var topelse=document.querySelector('.topelse')
+        topelse.style.display="none"
     }
     render() {
         return (
@@ -60,8 +92,33 @@ class header extends Component {
                             </div>
                         </div>
                         <div className="centerheart">
+
                              <a href="http://" target="_blank" rel="noopener noreferrer"> </a>
                         </div>
+                    </div>
+                     {/* 输入框 */}
+                    <div className="centerinput">
+                            <div className="kong"></div>
+                            <div className="input">
+                                <form>
+                                    <input type="text" value={this.state.inputValue} onChange={this.inputChange} onFocus={this.inputFous} onBlur={this.inputBlur} placeholder="这些艺人的另一个身份是铲屎官"/>
+                                </form>
+                            </div>
+                            <button></button>
+                            {/* 搜索排行榜 */}
+                            <div className="topelse">
+                                <ul>
+                                    {
+                                        this.state.top.map((value,key)=>{
+                                            return (
+                                                <li key={key}>
+                                                    <div className="number">{key+1}</div> {value.title}
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
                     </div>
                 </div>
             </div>
